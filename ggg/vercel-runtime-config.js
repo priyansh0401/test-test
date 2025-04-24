994257c9c7ca263b976a251f070c6b42dcf53236
priyansh0401 <priyansh0401@icloud.com>
2025-04-24 08:31:21 +0530

    m

--- ggg/vercel-runtime-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // This file is used to configure the Vercel runtime
-    // See: https://vercel.com/docs/runtimes#official-runtimes/python
-    pythonVersion: "3.9",
-  }
-  
